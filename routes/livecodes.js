92da6311ed19b7a86016236297f712c941da59fa
Hanan Wiener <hanan888@gmail.com>
2022-12-07 12:40:02 +0200

    create skeleton of endpoint

---
+++ routes/livecodes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+//validate the input
+//create and save the liveCode object in MongoDB
+//return response (with the created object)
+});
